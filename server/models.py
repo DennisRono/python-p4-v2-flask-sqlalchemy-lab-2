532cb1e7a27c4301599a8402a4f8dd7cfe30bf5d
Dennis Kibet <dennisrkibet@gmail.com>
2024-10-03 21:53:08 +0300

    migrations

--- server/models.py
+++ server/models.py
@@ -1,32 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
-    __tablename__ = 'customers'
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    comment = db.Column(db.String, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
 
-    def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+    customer = relationship("Customer", back_populates="reviews")
+    item = relationship("Item", back_populates="reviews")
 
 
-class Item(db.Model):
-    __tablename__ = 'items'
-
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    price = db.Column(db.Float)
+    name = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+    reviews = relationship("Review", back_populates="customer")
+
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    reviews = relationship("Review", back_populates="item")
